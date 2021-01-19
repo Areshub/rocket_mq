ebe8b54bf18b52234ba1626858fc18b26e836867
linanfei@gistone.cn <linanfei123456>
2021-01-19 16:35:15 +0800

    create

---
+++ src/test/java/com/lnf/rocket_mq/RocketMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnf.rocket_mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
